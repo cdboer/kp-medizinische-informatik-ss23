63f0a25432efe735d7a582ede2a503be46bc2110
Jonas <me@jonashoepner.de>
2023-05-25 22:30:50 +0200

    new plotter queriws

---
+++ eda/plotter/allICUages.py
@@ -0,0 +1,44 @@
+import psycopg2
+import matplotlib.pyplot as plt
+
+# Connection parameters
+hostname = '***REMOVED***'
+port = 5432
+database = 'mimiciv'
+username = 'mimicuser'
+password = '***REMOVED***'
+
+print(" Connect to the PostgreSQL server")
+conn = psycopg2.connect(
+    host=hostname,
+    port=port,
+    dbname=database,
+    user=username,
+    password=password
+)
+
+# Create a cursor object
+cur = conn.cursor()
+
+# Execute a SELECT query on the "example" table
+print("Executing query...")
+#cur.execute('SELECT a.age::INTEGER, count(distinct a.subject_id) FROM mimiciv_hosp.patients p JOIN mimiciv_derived.age a on p.subject_id = a.subject_id JOIN mimiciv_derived.sepsis3 s on p.subject_id = s.subject_id and s.sepsis3 WHERE p.subject_id in (	SELECT subject_id from mimiciv_icu.icustays ) GROUP BY a.age::INTEGER ORDER BY a.age::INTEGER asc;')
+cur.execute('SELECT a.age::INTEGER, count(distinct a.subject_id) FROM mimiciv_hosp.patients p JOIN mimiciv_derived.age a on p.subject_id = a.subject_id WHERE (p.subject_id in (select subject_id from mimiciv_icu.icustays)) GROUP BY a.age::INTEGER ORDER BY a.age::INTEGER asc;')
+print("fetching results")
+# Fetch and print the results
+results = cur.fetchall()	
+print("Fetched " + str(len(results)) + "rows.")
+mapped = list(map(list, zip(*results)))
+print(mapped)
+for row in mapped:
+    print(row)
+plt.plot(mapped[0], mapped[1])
+plt.title('Age of all ICU patients')
+plt.xlabel('Age')
+plt.ylabel('Count')
+
+plt.show()
+plt.savefig('allICUages.png')
+# Close the cursor and connection
+cur.close()
+conn.close()
