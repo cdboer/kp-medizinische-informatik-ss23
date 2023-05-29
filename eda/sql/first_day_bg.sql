c80677ebdf581710efb57a288d5a29f356c587bc
cdboer <dev@claas.plus>
2023-05-29 19:02:19 +0200

    Update first day queries

--- eda/sql/first_day_bg.sql
+++ eda/sql/first_day_bg.sql
@@ -1,33 +1,104 @@
-WITH sepsis AS (SELECT stay_id FROM mimiciv_derived.sepsis3)
-SELECT
-    MIN(lactate) AS lactate_min, MAX(lactate) AS lactate_max, AVG(lactate) AS lactate_mean, STDDEV(lactate) AS lactate_std
-    , MIN(ph) AS ph_min, MAX(ph) AS ph_max, AVG(ph) AS ph_mean, STDDEV(ph) AS ph_std
-    , MIN(so2) AS so2_min, MAX(so2) AS so2_max, AVG(so2) AS so2_mean, STDDEV(so2) AS so2_std
-    , MIN(po2) AS po2_min, MAX(po2) AS po2_max, AVG(po2) AS po2_mean, STDDEV(po2) AS po2_std
-    , MIN(pco2) AS pco2_min, MAX(pco2) AS pco2_max, AVG(pco2) AS pco2_mean, STDDEV(pco2) AS pco2_std
-    , MIN(aado2) AS aado2_min, MAX(aado2) AS aado2_max, AVG(aado2) AS aado2_mean, STDDEV(aado2) AS aado2_std
-    , MIN(aado2_calc) AS aado2_calc_min, MAX(aado2_calc) AS aado2_calc_max, AVG(aado2_calc) AS aado2_calc_mean, STDDEV(aado2_calc) AS aado2_calc_std
-    , MIN(pao2fio2ratio) AS pao2fio2ratio_min 
-    , MAX(pao2fio2ratio) AS pao2fio2ratio_max, AVG(pao2fio2ratio) AS pao2fio2ratio_mean, STDDEV(pao2fio2ratio) AS pao2fio2ratio_std
-    , MIN(baseexcess) AS baseexcess_min, MAX(baseexcess) AS baseexcess_max, AVG(baseexcess) AS baseexcess_mean, STDDEV(baseexcess) AS baseexcess_std
-    , MIN(bicarbonate) AS bicarbonate_min, MAX(bicarbonate) AS bicarbonate_max, AVG(bicarbonate) AS bicarbonate_mean, STDDEV(bicarbonate) AS bicarbonate_std
-    , MIN(totalco2) AS totalco2_min, MAX(totalco2) AS totalco2_max, AVG(totalco2) AS totalco2_mean, STDDEV(totalco2) AS totalco2_std
-    , MIN(hematocrit) AS hematocrit_min, MAX(hematocrit) AS hematocrit_max, AVG(hematocrit) AS hematocrit_mean, STDDEV(hematocrit) AS hematocrit_std
-    , MIN(hemoglobin) AS hemoglobin_min, MAX(hemoglobin) AS hemoglobin_max, AVG(hemoglobin) AS hemoglobin_mean, STDDEV(hemoglobin) AS hemoglobin_std
-    , MIN(carboxyhemoglobin) AS carboxyhemoglobin_min
-    , MAX(carboxyhemoglobin) AS carboxyhemoglobin_max, AVG(carboxyhemoglobin) AS carboxyhemoglobin_mean, STDDEV(carboxyhemoglobin) AS carboxyhemoglobin_std
-    , MIN(methemoglobin) AS methemoglobin_min
-    , MAX(methemoglobin) AS methemoglobin_max, AVG(methemoglobin) AS methemoglobin_mean, STDDEV(methemoglobin) AS methemoglobin_std
-    , MIN(temperature) AS temperature_min, MAX(temperature) AS temperature_max, AVG(temperature) AS temperature_mean, STDDEV(temperature) AS temperature_std
-    , MIN(chloride) AS chloride_min, MAX(chloride) AS chloride_max, AVG(chloride) AS chloride_mean, STDDEV(chloride) AS chloride_std
-    , MIN(calcium) AS calcium_min, MAX(calcium) AS calcium_max, AVG(calcium) AS calcium_mean, STDDEV(calcium) AS calcium_std
-    , MIN(glucose) AS glucose_min, MAX(glucose) AS glucose_max, AVG(glucose) AS glucose_mean, STDDEV(glucose) AS glucose_std
-    , MIN(potassium) AS potassium_min, MAX(potassium) AS potassium_max, AVG(potassium) AS potassium_mean, STDDEV(potassium) AS potassium_std
-    , MIN(sodium) AS sodium_min, MAX(sodium) AS sodium_max, AVG(sodium) AS sodium_mean, STDDEV(sodium) AS sodium_std
-	, CASE WHEN ie.stay_id in (SELECT * FROM sepsis) THEN 1 ELSE 0 END AS sepsis
+/*
+Extract the first day of blood gas measurements for each ICU stay.
+ */
+WITH sepsis AS (
+    SELECT stay_id
+    FROM mimiciv_derived.sepsis3
+)
+SELECT MIN(lactate) AS lactate_min,
+    MAX(lactate) AS lactate_max,
+    AVG(lactate) AS lactate_mean,
+    STDDEV(lactate) AS lactate_std,
+    MIN(ph) AS ph_min,
+    MAX(ph) AS ph_max,
+    AVG(ph) AS ph_mean,
+    STDDEV(ph) AS ph_std,
+    MIN(so2) AS so2_min,
+    MAX(so2) AS so2_max,
+    AVG(so2) AS so2_mean,
+    STDDEV(so2) AS so2_std,
+    MIN(po2) AS po2_min,
+    MAX(po2) AS po2_max,
+    AVG(po2) AS po2_mean,
+    STDDEV(po2) AS po2_std,
+    MIN(pco2) AS pco2_min,
+    MAX(pco2) AS pco2_max,
+    AVG(pco2) AS pco2_mean,
+    STDDEV(pco2) AS pco2_std,
+    MIN(aado2) AS aado2_min,
+    MAX(aado2) AS aado2_max,
+    AVG(aado2) AS aado2_mean,
+    STDDEV(aado2) AS aado2_std,
+    MIN(aado2_calc) AS aado2_calc_min,
+    MAX(aado2_calc) AS aado2_calc_max,
+    AVG(aado2_calc) AS aado2_calc_mean,
+    STDDEV(aado2_calc) AS aado2_calc_std,
+    MIN(pao2fio2ratio) AS pao2fio2ratio_min,
+    MAX(pao2fio2ratio) AS pao2fio2ratio_max,
+    AVG(pao2fio2ratio) AS pao2fio2ratio_mean,
+    STDDEV(pao2fio2ratio) AS pao2fio2ratio_std,
+    MIN(baseexcess) AS baseexcess_min,
+    MAX(baseexcess) AS baseexcess_max,
+    AVG(baseexcess) AS baseexcess_mean,
+    STDDEV(baseexcess) AS baseexcess_std,
+    MIN(bicarbonate) AS bicarbonate_min,
+    MAX(bicarbonate) AS bicarbonate_max,
+    AVG(bicarbonate) AS bicarbonate_mean,
+    STDDEV(bicarbonate) AS bicarbonate_std,
+    MIN(totalco2) AS totalco2_min,
+    MAX(totalco2) AS totalco2_max,
+    AVG(totalco2) AS totalco2_mean,
+    STDDEV(totalco2) AS totalco2_std,
+    MIN(hematocrit) AS hematocrit_min,
+    MAX(hematocrit) AS hematocrit_max,
+    AVG(hematocrit) AS hematocrit_mean,
+    STDDEV(hematocrit) AS hematocrit_std,
+    MIN(hemoglobin) AS hemoglobin_min,
+    MAX(hemoglobin) AS hemoglobin_max,
+    AVG(hemoglobin) AS hemoglobin_mean,
+    STDDEV(hemoglobin) AS hemoglobin_std,
+    MIN(carboxyhemoglobin) AS carboxyhemoglobin_min,
+    MAX(carboxyhemoglobin) AS carboxyhemoglobin_max,
+    AVG(carboxyhemoglobin) AS carboxyhemoglobin_mean,
+    STDDEV(carboxyhemoglobin) AS carboxyhemoglobin_std,
+    MIN(methemoglobin) AS methemoglobin_min,
+    MAX(methemoglobin) AS methemoglobin_max,
+    AVG(methemoglobin) AS methemoglobin_mean,
+    STDDEV(methemoglobin) AS methemoglobin_std,
+    MIN(temperature) AS temperature_min,
+    MAX(temperature) AS temperature_max,
+    AVG(temperature) AS temperature_mean,
+    STDDEV(temperature) AS temperature_std,
+    MIN(chloride) AS chloride_min,
+    MAX(chloride) AS chloride_max,
+    AVG(chloride) AS chloride_mean,
+    STDDEV(chloride) AS chloride_std,
+    MIN(calcium) AS calcium_min,
+    MAX(calcium) AS calcium_max,
+    AVG(calcium) AS calcium_mean,
+    STDDEV(calcium) AS calcium_std,
+    MIN(glucose) AS glucose_min,
+    MAX(glucose) AS glucose_max,
+    AVG(glucose) AS glucose_mean,
+    STDDEV(glucose) AS glucose_std,
+    MIN(potassium) AS potassium_min,
+    MAX(potassium) AS potassium_max,
+    AVG(potassium) AS potassium_mean,
+    STDDEV(potassium) AS potassium_std,
+    MIN(sodium) AS sodium_min,
+    MAX(sodium) AS sodium_max,
+    AVG(sodium) AS sodium_mean,
+    STDDEV(sodium) AS sodium_std,
+    CASE
+        WHEN ie.stay_id in (
+            SELECT *
+            FROM sepsis
+        ) THEN 1
+        ELSE 0
+    END AS sepsis
 FROM mimiciv_icu.icustays ie
-LEFT JOIN mimiciv_derived.bg bg
-    ON ie.subject_id = bg.subject_id
-        AND bg.charttime >= ie.intime - INTERVAL '6' HOUR
-        AND bg.charttime <= ie.intime + INTERVAL '1' DAY
-GROUP BY ie.subject_id, ie.stay_id
+    LEFT JOIN mimiciv_derived.bg bg ON ie.subject_id = bg.subject_id
+    AND bg.charttime >= ie.intime - INTERVAL '6' HOUR
+    AND bg.charttime <= ie.intime + INTERVAL '1' DAY
+GROUP BY ie.subject_id,
+    ie.stay_id
--- eda/sql/first_day_vitalsign.sql
+++ eda/sql/first_day_vitalsign.sql
@@ -1,79 +1,52 @@
--- WITH sepsis AS (SELECT stay_id FROM mimiciv_derived.sepsis3)
--- SELECT 
-	-- vs.heart_rate_min, 
-	-- vs.heart_rate_max, 
-	-- vs.heart_rate_mean,
-	-- vs.sbp_min,
-	-- vs.sbp_max,
-	-- vs.sbp_mean,
-	-- vs.dbp_min,
-	-- vs.dbp_max,
-	-- vs.dbp_mean,
-	-- vs.mbp_min,
-	-- vs.mbp_max,
-	-- vs.mbp_mean,
-	-- vs.temperature_min::integer,
-	-- vs.temperature_max::integer,
-	-- vs.temperature_mean::integer,
-	-- vs.spo2_min,
-	-- vs.spo2_max,
-	-- vs.spo2_mean,
-	-- vs.glucose_min,
-	-- vs.glucose_max,
-	-- vs.glucose_mean,
-	-- vs.resp_rate_min,
-	-- vs.resp_rate_max,
-	-- vs.resp_rate_mean,
-	-- CASE WHEN icu.stay_id in (SELECT * FROM sepsis) THEN 1 ELSE 0 END AS sepsis
--- FROM mimiciv_icu.icustays icu
--- LEFT JOIN mimiciv_derived.first_day_vitalsign vs
--- ON icu.stay_id = vs.stay_id
-
-WITH sepsis AS (SELECT stay_id FROM mimiciv_derived.sepsis3)
-SELECT
-    MIN(heart_rate) AS heart_rate_min
-    , MAX(heart_rate) AS heart_rate_max
-    , AVG(heart_rate) AS heart_rate_mean
-    , STDDEV(heart_rate) AS heart_rate_std
-    , MIN(sbp) AS sbp_min
-    , MAX(sbp) AS sbp_max
-    , AVG(sbp) AS sbp_mean
-    , STDDEV(sbp) AS sbp_std
-    , MIN(dbp) AS dbp_min
-    , MAX(dbp) AS dbp_max
-    , AVG(dbp) AS dbp_mean
-    , STDDEV(dbp) AS dbp_std
-    , MIN(mbp) AS mbp_min
-    , MAX(mbp) AS mbp_max
-    , AVG(mbp) AS mbp_mean
-    , STDDEV(mbp) AS mbp_std
-    , MIN(resp_rate) AS resp_rate_min
-    , MAX(resp_rate) AS resp_rate_max
-    , AVG(resp_rate) AS resp_rate_mean
-    , STDDEV(resp_rate) AS resp_rate_std
-    , MIN(temperature) AS temperature_min
-    , MAX(temperature) AS temperature_max
-    , AVG(temperature) AS temperature_mean
-    , STDDEV(temperature) AS temperature_std
-    , MIN(spo2) AS spo2_min
-    , MAX(spo2) AS spo2_max
-    , AVG(spo2) AS spo2_mean
-    , STDDEV(spo2) AS spo2_std
-    , MIN(glucose) AS glucose_min
-    , MAX(glucose) AS glucose_max
-    , AVG(glucose) AS glucose_mean
-    , STDDEV(glucose) AS glucose_std
-    , CASE WHEN ie.stay_id in (SELECT * FROM sepsis) THEN 1 ELSE 0 END AS sepsis
+/*
+ Extracts the first day vitalsigns for each ICU stay.
+ */
+WITH sepsis AS (
+    SELECT stay_id
+    FROM mimiciv_derived.sepsis3
+)
+SELECT MIN(heart_rate) AS heart_rate_min,
+    MAX(heart_rate) AS heart_rate_max,
+    AVG(heart_rate) AS heart_rate_mean,
+    STDDEV(heart_rate) AS heart_rate_std,
+    MIN(sbp) AS sbp_min,
+    MAX(sbp) AS sbp_max,
+    AVG(sbp) AS sbp_mean,
+    STDDEV(sbp) AS sbp_std,
+    MIN(dbp) AS dbp_min,
+    MAX(dbp) AS dbp_max,
+    AVG(dbp) AS dbp_mean,
+    STDDEV(dbp) AS dbp_std,
+    MIN(mbp) AS mbp_min,
+    MAX(mbp) AS mbp_max,
+    AVG(mbp) AS mbp_mean,
+    STDDEV(mbp) AS mbp_std,
+    MIN(resp_rate) AS resp_rate_min,
+    MAX(resp_rate) AS resp_rate_max,
+    AVG(resp_rate) AS resp_rate_mean,
+    STDDEV(resp_rate) AS resp_rate_std,
+    MIN(temperature) AS temperature_min,
+    MAX(temperature) AS temperature_max,
+    AVG(temperature) AS temperature_mean,
+    STDDEV(temperature) AS temperature_std,
+    MIN(spo2) AS spo2_min,
+    MAX(spo2) AS spo2_max,
+    AVG(spo2) AS spo2_mean,
+    STDDEV(spo2) AS spo2_std,
+    MIN(glucose) AS glucose_min,
+    MAX(glucose) AS glucose_max,
+    AVG(glucose) AS glucose_mean,
+    STDDEV(glucose) AS glucose_std,
+    CASE
+        WHEN ie.stay_id in (
+            SELECT *
+            FROM sepsis
+        ) THEN 1
+        ELSE 0
+    END AS sepsis
 FROM mimiciv_icu.icustays ie
-LEFT JOIN mimiciv_derived.sepsis3 sepsis
-    ON ie.stay_id = sepsis.stay_id
-LEFT JOIN mimiciv_derived.vitalsign vs
-    ON ie.subject_id = vs.subject_id
-    -- pre septic window defined as 8 hours before sepsis onset
-    -- if no sepsis onset, then 8 hours after ICU admission to get the same window size
-    -- resolution could be improved ..
-		AND vs.charttime >= ie.intime -  INTERVAL '6' HOUR
-        AND vs.charttime <= ie.intime + INTERVAL '1' DAY
-        -- AND vs.charttime >= (CASE WHEN sepsis.sofa_time IS NULL THEN ie.intime ELSE sepsis.sofa_time - INTERVAL '8' HOUR END)
-        -- AND vs.charttime <= (CASE WHEN sepsis.sofa_time IS NULL THEN ie.intime + INTERVAL '8' HOUR ELSE sepsis.sofa_time END)
-GROUP BY ie.subject_id, ie.stay_id;
+    LEFT JOIN mimiciv_derived.vitalsign vs ON ie.subject_id = vs.subject_id
+    AND vs.charttime >= ie.intime - INTERVAL '6' HOUR
+    AND vs.charttime <= ie.intime + INTERVAL '1' DAY
+GROUP BY ie.subject_id,
+    ie.stay_id;
