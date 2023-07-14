f2d8186c39f4bda82600fd20f8bbea4274675391
Jonas <me@jonashoepner.de>
2023-07-14 12:15:01 +0200

    cohort selection query

---
+++ ml/sql/cohort_selection.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS mimiciv_derived.cohort_selection;
+CREATE TABLE mimiciv_derived.cohort_selection AS
+SELECT ie.stay_id
+from mimiciv_icu.icustays ie
+LEFT JOIN mimiciv_derived.sepsis3 s
+	on s.stay_id = ie.stay_id
+where ie.outtime - ie.intime >= INTERVAL '8' hour
+and case when s.sofa_time is not null then s.sofa_time - ie.intime >= INTERVAL '8' hour else true end;
--- ml/sql/sepsis_with_rdm_onset_times.sql
+++ ml/sql/sepsis_with_rdm_onset_times.sql
@@ -1,18 +1,20 @@
 DROP TABLE IF EXISTS mimiciv_derived.sepsis_with_rdm_onset_time;
 CREATE TABLE mimiciv_derived.sepsis_with_rdm_onset_time AS 
 SELECT 
-ie.stay_id, 
+cs.stay_id,
 CASE WHEN s.sofa_time is null 
-    THEN (SELECT (ie.intime + (sofa_time - ie2.intime)) as sofa_time from mimiciv_derived.sepsis3 s
-          LEFT JOIN mimiciv_icu.icustays ie2 on ie2.stay_id = s.stay_id  
-		  where sofa_time is not null 
-          AND ((sofa_time - ie2.intime) > INTERVAL '8' hour)
+    THEN (SELECT (ie.intime + (s.sofa_time - ie2.intime)) as sofa_time from mimiciv_derived.cohort_selection cs
+          JOIN mimiciv_derived.sepsis3 s on cs.stay_id = s.stay_id  
+          JOIN mimiciv_icu.icustays ie2 on ie2.stay_id = cs.stay_id
+		  where s.sofa_time is not null 
+          AND ((s.sofa_time - ie2.intime) >= INTERVAL '8' hour)
           ORDER BY RANDOM() LIMIT 1
         ) 
     else s.sofa_time 
 end,
 CASE when s.sepsis3 is null then 0 else 1 end
-FROM mimiciv_icu.icustays ie
-LEFT JOIN mimiciv_derived.sepsis3 s on s.stay_id = ie.stay_id
+FROM mimiciv_derived.cohort_selection cs
+LEFT JOIN mimiciv_derived.sepsis3 s on s.stay_id = cs.stay_id
+LEFT JOIN mimiciv_icu.icustays ie on ie.stay_id = cs.stay_id
 ;
 
