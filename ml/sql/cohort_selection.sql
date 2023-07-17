be0a3badb80461cc0248c2fd2b214debdc8043e5
Jonas <jonas.hoepner@mailbox.tu-dresden.de>
2023-07-17 13:40:16 +0200

    many fixes due to merge

--- ml/sql/cohort_selection.sql
+++ ml/sql/cohort_selection.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS mimiciv_derived.cohort_selection;
 CREATE TABLE mimiciv_derived.cohort_selection AS
-SELECT ie.stay_id
+SELECT ie.stay_id, ie.subject_id
 from mimiciv_icu.icustays ie
 LEFT JOIN mimiciv_derived.sepsis3 s
 	on s.stay_id = ie.stay_id
